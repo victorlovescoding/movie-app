7f5254dc726331ef92fa88852727681c2dfe5aa6
Victorchen <96483328+victorlovescoding@users.noreply.github.com>
2023-04-13 21:31:24 +0800

    restructure css with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    boxShadow: {
+      DEFAULT: '0 4px 5px rgba(0, 0, 0, 0.2)',
+    },
+  },
+  plugins: [],
+}
+
